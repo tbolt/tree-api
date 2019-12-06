a21bb2dd7a7e5c38d75520e1c68e7e3763f4f470
Ty Bolchoz <tybolt@gmail.com>
2019-12-06 14:34:07 -0500

    swap database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_12_06_170211) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.binary "image"
     t.text "description"
